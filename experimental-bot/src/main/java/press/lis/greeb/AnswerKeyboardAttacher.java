6320641732ce37423c1feef5ad7e44e698701322
fhtagneast <vchernogorov@yandex.ru>
2020-02-11 00:15:06 +0300

    #2 Refactored keyboard class

--- experimental-bot/src/main/java/press/lis/greeb/AnswerKeyboardAttacher.java
+++ experimental-bot/src/main/java/press/lis/greeb/AnswerKeyboardAttacher.java
@@ -8,18 +8,26 @@ import java.util.ArrayList;
 
 public class AnswerKeyboardAttacher {
 
+    private static ArrayList<ArrayList<String>> buttonTexts = new ArrayList<>(2);
+
+    static {
+        buttonTexts.add(new ArrayList<>(2));
+        buttonTexts.add(new ArrayList<>(1));
+        buttonTexts.get(0).add("Will go!");
+        buttonTexts.get(0).add("Wouldn't go =(");
+        buttonTexts.get(1).add("Dunno...");
+    }
+
     public static SendMessage attachKeyboard(SendMessage message) {
 
-        KeyboardRow row1 = new KeyboardRow();
-        row1.add(0, "Will go!");
-        row1.add(1, "Wouldn't go =(");
-
-        KeyboardRow row2 = new KeyboardRow();
-        row2.add(0, "Dunno...");
-
-        ArrayList<KeyboardRow> rowArrayList = new ArrayList<>();
-        rowArrayList.add(row1);
-        rowArrayList.add(row2);
+        ArrayList<KeyboardRow> rowArrayList = new ArrayList<>(buttonTexts.size());
+        for (ArrayList<String> buttonText : buttonTexts) {
+            KeyboardRow row = new KeyboardRow();
+            for (int j = 0; j < buttonText.size(); j++) {
+                row.add(j, buttonText.get(j));
+            }
+            rowArrayList.add(row);
+        }
 
         ReplyKeyboardMarkup keyboard = new ReplyKeyboardMarkup()
                 .setKeyboard(rowArrayList)
@@ -29,4 +37,5 @@ public class AnswerKeyboardAttacher {
         return message.setReplyMarkup(keyboard);
 
     }
+
 }
