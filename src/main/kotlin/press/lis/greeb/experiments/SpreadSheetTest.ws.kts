498cec85f3bec14ef4f5bd590bcd5973d333dd42
Eliseev Alexandr <eliseealex@gmail.com>
2020-03-08 01:00:48 +0300

    Validation of idea of writing in the sheets + plan for bot for HammerTime

--- src/main/kotlin/press/lis/greeb/experiments/SpreadSheetTest.ws.kts
+++ src/main/kotlin/press/lis/greeb/experiments/SpreadSheetTest.ws.kts
@@ -1,5 +1,6 @@
 package press.lis.greeb.experiments
 
+import com.google.api.services.sheets.v4.model.ValueRange
 import press.lis.greeb.SheetsClient
 
 // You can try to run this in REPL mode to experiment with spreadsheets
@@ -7,8 +8,9 @@ print("Testss")
 
 val x = SheetsClient.getSheetService()
 
+val s_id = "1q3pPL_PMhnXbaAOsQ4EU96hAn7ZJuZQJguEEyvONipY"
 val response = x.spreadsheets().values()
-        .get("1u1pQx3RqqOFX-rr3Wuajyts_ufCeIQ21Mu0ndXCdv2M",
+        .get(s_id,
                 "A:Z")
         .execute()
 
@@ -16,3 +18,15 @@ val values = response.getValues()
 
 values[0]
 values[1]
+
+x.spreadsheets().values().update(
+        s_id,
+        "AJ9",
+        ValueRange().setValues(listOf(listOf(true)))
+).execute()
+
+x.spreadsheets().values().update(
+        s_id,
+        "AJ9",
+        ValueRange().setValues(listOf(listOf(true)))
+).execute()
