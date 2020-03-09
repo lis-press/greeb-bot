dc3a29672e53253d72bc5383a723af930c2f1c10
Eliseev Alexandr <eliseealex@gmail.com>
2020-03-09 13:00:04 +0300

    #18 HammerTime Marathon Bot:
    Designed and implemented the state for subscription
    Added the broadcast message to subscribed users

--- src/main/kotlin/press/lis/greeb/experiments/SpreadSheetTest.ws.kts
+++ src/main/kotlin/press/lis/greeb/experiments/SpreadSheetTest.ws.kts
@@ -1,32 +1,32 @@
 package press.lis.greeb.experiments
 
-import com.google.api.services.sheets.v4.model.ValueRange
 import press.lis.greeb.SheetsClient
 
 // You can try to run this in REPL mode to experiment with spreadsheets
-print("Testss")
+print("Test")
 
 val x = SheetsClient.getSheetService()
 
 val s_id = "1q3pPL_PMhnXbaAOsQ4EU96hAn7ZJuZQJguEEyvONipY"
 val response = x.spreadsheets().values()
         .get(s_id,
-                "A:Z")
+                "A:AM")
         .execute()
+
+//x.spreadsheets().values().update(
+//        s_id,
+//        "AJ9",
+//        ValueRange().setValues(listOf(listOf(true)))
+//).execute()
 
 val values = response.getValues()
 
-values[0]
-values[1]
+val subscribed = values.subList(1, values.size - 1).filter {
+    it.size > 2 && it[2] != null && it[2] != ""
+}
 
-x.spreadsheets().values().update(
-        s_id,
-        "AJ9",
-        ValueRange().setValues(listOf(listOf(true)))
-).execute()
+subscribed
 
-x.spreadsheets().values().update(
-        s_id,
-        "AJ9",
-        ValueRange().setValues(listOf(listOf(true)))
-).execute()
+println(subscribed)
+
+subscribed.map { it.size }
