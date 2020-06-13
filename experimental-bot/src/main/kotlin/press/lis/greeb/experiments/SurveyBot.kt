f26c46b2863a19e918cc6fa790e1052bcf0a0354
Eliseev Alexandr <eliseealex@gmail.com>
2020-06-13 18:22:20 +0300

    #35 Survey Bot research
    * Made an experiment with cross-chat posting

--- experimental-bot/src/main/kotlin/press/lis/greeb/experiments/SurveyBot.kt
+++ experimental-bot/src/main/kotlin/press/lis/greeb/experiments/SurveyBot.kt
@@ -2,6 +2,7 @@ package press.lis.greeb.experiments
 
 import com.typesafe.config.ConfigFactory
 import mu.KotlinLogging
+import org.apache.commons.codec.binary.Base64
 import org.telegram.telegrambots.ApiContextInitializer
 import org.telegram.telegrambots.bots.DefaultBotOptions
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
@@ -9,12 +10,16 @@ import org.telegram.telegrambots.meta.ApiContext
 import org.telegram.telegrambots.meta.TelegramBotsApi
 import org.telegram.telegrambots.meta.api.methods.AnswerInlineQuery
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText
+import org.telegram.telegrambots.meta.api.objects.Message
 import org.telegram.telegrambots.meta.api.objects.Update
 import org.telegram.telegrambots.meta.api.objects.inlinequery.inputmessagecontent.InputTextMessageContent
 import org.telegram.telegrambots.meta.api.objects.inlinequery.result.InlineQueryResultArticle
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton
+import java.nio.ByteBuffer
+import java.nio.charset.Charset
 import java.util.*
 
 /**
@@ -44,23 +49,38 @@ class HammerTimeMarathonBot(botToken: String, options: DefaultBotOptions?) : Tel
                 val chatId = update.message.chatId
 
                 // TODO Так, свитч инлайн я смог настроить здесь, пока не понимаю, как это всё
+
+                val result = execute(SendMessage()
+                        .setChatId(chatId)
+//                        .setReplyMarkup(inlineKeyboardMarkup)
+                        .setText(update.message?.text))
+
+
+                val test = ByteBuffer.allocate(Int.SIZE_BYTES + Long.SIZE_BYTES)
+                        .putInt(result.messageId)
+                        .putLong(result.chatId)
+                        .array()
+
                 val inlineKeyboardMarkup = InlineKeyboardMarkup(
                         listOf(
                                 listOf(
                                         InlineKeyboardButton("First").setCallbackData("F")
                                 ),
                                 listOf(
-                                        InlineKeyboardButton("Third").setSwitchInlineQuery("Testing test")
+                                        InlineKeyboardButton("Third").setSwitchInlineQuery(
+                                                Base64.encodeBase64(test)
+                                                        .toString(Charset.forName("UTF8")))
                                 )
                         ))
 
-                val result = execute(SendMessage()
-                        .setChatId(chatId)
-                        .setReplyMarkup(inlineKeyboardMarkup)
-                        .setText("Сообщение!"))
+                // Don't need it actually
+                execute(EditMessageReplyMarkup()
+                        .setChatId(result.chatId)
+                        .setMessageId(result.messageId)
+                        .setReplyMarkup(inlineKeyboardMarkup))
 
                 // TODO я могу попытаться здесь сразу отредактировать это сообщение...
-                // TODO не уверен
+                // TODO не уверен, есть ли здесь какие-то ещё интересные варианты
             }
             update.hasCallbackQuery() -> {
 
@@ -83,20 +103,52 @@ class HammerTimeMarathonBot(botToken: String, options: DefaultBotOptions?) : Tel
                         .setText(callbackQuery.message.text + "\nCallback ${callbackQuery.data} получен"))
             }
             update.hasInlineQuery() -> {
+
+
+                val messageAndChatByteArray = Base64.decodeBase64(update.inlineQuery.query)
+                val messageAndChatByteBuffer = ByteBuffer.wrap(messageAndChatByteArray)
+
+                val messageId = messageAndChatByteBuffer.getInt(0)
+                val chatId = messageAndChatByteBuffer.getLong(Int.SIZE_BYTES)
+
+                // TODO duplication -> кажется, что здесь нельзя поменять опрос на тот же или всё-таки можно, я так и не понял
+                val inlineKeyboardMarkup = InlineKeyboardMarkup(
+                        listOf(
+                                listOf(
+                                        InlineKeyboardButton("First").setCallbackData("FF")
+                                ),
+                                listOf(
+                                        InlineKeyboardButton("Third").setSwitchInlineQuery(
+                                                update.inlineQuery.query)
+                                )
+                        ))
+
+                val messageProcessed = execute(EditMessageReplyMarkup()
+                        .setChatId(chatId)
+                        .setMessageId(messageId)
+                        .setReplyMarkup(inlineKeyboardMarkup))
+
+                val results: List<InlineQueryResultArticle> = if (messageProcessed is Message) {
+                    val messageProcessedInternal = messageProcessed as Message
+                    listOf(InlineQueryResultArticle()
+                            .setId(UUID.randomUUID().toString())
+                            .setHideUrl(true)
+                            .setTitle("Title test")
+                            .setDescription("Description test")
+                            .setInputMessageContent(InputTextMessageContent()
+                                    .setMessageText(messageProcessedInternal.text)
+                                    .setDisableWebPagePreview(true))
+                            .setUrl("http://nfclub.tilda.ws/"))
+                } else {
+                    listOf()
+                }
+
                 execute(AnswerInlineQuery()
                         .setInlineQueryId(update.inlineQuery.id)
                         .setSwitchPmText("Тоже хочу завести подобный опрос")
                         .setSwitchPmParameter("Test")
 //                    .setResults(listOf())
-                        .setResults(InlineQueryResultArticle()
-                                .setId(UUID.randomUUID().toString())
-                                .setHideUrl(true)
-                                .setTitle("Title test")
-                                .setDescription("Description test")
-                                .setInputMessageContent(InputTextMessageContent()
-                                        .setMessageText("*Test*")
-                                        .setDisableWebPagePreview(true))
-                                .setUrl("http://nfclub.tilda.ws/"))
+                        .setResults(results)
                 )
             }
             else -> {
